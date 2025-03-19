901fc6d7102937c5032e4691b3d03d0b5fc35743
MateusCarvalho2007 <mateusreichert2007@gmail.com>
2025-03-19 14:48:49 -0300

    att x

---
+++ Formulario/script.js
@@ -0,0 +1,30 @@
+// Vamos iniciar imprimindo um conteúdo no console do navegador
+console.log("oi");
+
+/*Em Javascript existem 3 tipos de escopo de variável:
+ var - global
+ let - escopo local
+ const - constante
+
+No Javascript há tipos primitivos e objetos. Os tipos primitivos são imutáveis e são atribuídos por valor.
+Já os objetos são atribuídos por referência
+
+*/
+
+/* String */
+let nome = "João";
+let nome2 = 'Maria';
+let mensagem = `Olá, ${nome}`;
+console.log(mensagem);
+
+
+/* Number */
+let idade = 25;       
+let valor = 99.99;    
+let infinito = Infinity;  
+let notANumber = NaN; 
+
+/* Boolean */
+let verdadeiro  = true;
+let falso = false;
+
