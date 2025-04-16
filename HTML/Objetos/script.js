db70447def91f68301832d3e0c62ac2407e99949
MateusCarvalho2007 <mateusreichert2007@gmail.com>
2025-04-16 14:54:39 -0300

    att11

--- HTML/Objetos/script.js
+++ HTML/Objetos/script.js
@@ -41,11 +41,16 @@ times.forEach(function(time){
     lista_times.appendChild(li);
 });
 
-const tres = document.getElementById("times_3_libertadores");      
-let tri = times.filter(p => p.libertadores >= 2);
-tri.forEach(function(time){
-    lista_tri = document.getElementById("times_3_libertadores");
-    const li = document.createElement("li")
-    li.textContent = time.nome;
-    lista_tri.appendChild(li);
-});
+//Usando filter, map e reduce juntos
+preços = [
+    {alimento:"Leite",preço:2.68,status:"perecível"},
+    {alimento:"Feijão",preço:6.99,status:"não perecível"},
+    {alimento:"Arroz",preço:6.75,status:"não perecível"},
+    {alimento:"Manteiga",preço:3.29,status:"perecível"},
+]
+
+let total_perecivel = preços
+    .filter(produto=>produto.status=="perecível")
+    .map(produto=>produto.preço*1.10)
+    .reduce((total,produto) => total+produto,0)
+console.log(`Produtos perecíveis: R$ ${total_perecivel.toFixed(2)}`)
